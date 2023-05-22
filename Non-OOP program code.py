1f64976692755cd4d070a7f4a70ae30dbd1340b0
Ms Lawliet <alexieanoya@gmail.com>
2023-05-22 22:55:05 +0800

    wrote pseudocode for non-oop program

---
+++ Non-OOP program code.py
@@ -0,0 +1,12 @@
+# Create a Simple App Calculator
+
+# ask for operation
+# ask user for two numbers
+# perform operation
+# for addition,
+# for subtraction,
+# for multiplication,
+# for division,
+# ask if user wants to retry
+# if yes, repeat process
+# if no, print "Thank you!"
