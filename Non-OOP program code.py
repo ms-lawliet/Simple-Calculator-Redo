e1754acb48771d1919e46005420ec551bacc59a4
Ms Lawliet <alexieanoya@gmail.com>
2023-05-22 23:19:11 +0800

    applied exception handling

--- Non-OOP program code.py
+++ Non-OOP program code.py
@@ -2,31 +2,41 @@
 
 while True:
     # ask for operation
-    operation = input("Enter operation to be used: ")
-    # ask for user input
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+    operation = input("Enter operation to be used: ").lower()
+    try:
+        # ask for user input
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Invalid input.")
 
-    # perform operation
-    # for addition,
-    if operation == "addition":
-        sum = num1 + num2
-        print(f"The sum is {sum}.")
-    # for subtraction,
-    elif operation == "subtraction":
-        difference = num1 - num2
-        print(f"The difference is {difference}.")
-    # for multiplication,
-    elif operation == "multiplication":
-        product = num1 * num2
-        print(f"The product is {product}.")
-    # for division,
-    elif operation == "division":
-        quotient = num1 / num2
-        print(f"The quotient is {quotient}.")
-    # else, invalid operation
-    else:
-        print("Operation is invalid.")
+    try:
+        # perform operation
+        # for addition,
+        if operation == "addition":
+            sum = num1 + num2
+            print(f"The sum is {sum}.")
+        # for subtraction,
+        elif operation == "subtraction":
+            difference = num1 - num2
+            print(f"The difference is {difference}.")
+        # for multiplication,
+        elif operation == "multiplication":
+            product = num1 * num2
+            print(f"The product is {product}.")
+        # for division,
+        elif operation == "division":
+            quotient = num1 / num2
+            print(f"The quotient is {quotient}.")
+        # else, invalid operation
+        else:
+            print("Operation is invalid.")
+    except TypeError:
+        print("Cannot perform operation due to invalid input.")
+    except NameError:
+        print("Cannot perform operation due to invalid input.")
+    except ZeroDivisionError:
+        print("Zero Division Error.")
 
     # ask if user wants to retry
     retry = input("Do you want to try again? (yes or no) ").lower()
@@ -37,3 +47,5 @@ while True:
     elif retry == "no":
         print("Thank you!")
         break
+    else:
+        print("Yes or no only.")
