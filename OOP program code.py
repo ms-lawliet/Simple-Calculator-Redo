ff228df011587833908cd4ff4e4137c8af3f0673
Ms Lawliet <alexieanoya@gmail.com>
2023-05-22 23:45:45 +0800

    asked for operation to be used

--- OOP program code.py
+++ OOP program code.py
@@ -1,6 +1,10 @@
 # import files
+from UserInterface import UserInterface
 # assign class methods to variables
+ui = UserInterface()
+
 # ask for input by calling methods
+operation = ui.ask_operation()
 # create instances for operations
 # call method to print result
 # call method for retry
--- UserInterface.py
+++ UserInterface.py
@@ -1,6 +1,10 @@
-# ask for operation
-# ask for user input
-# display result
-# ask if user wants to retry
-# if yes, repeat process
-# if no, print "Thank you!"
+class UserInterface:
+    def ask_operation(self):
+        # ask for operation
+        operation = input("Which operation do you want to perform? ").lower()
+        print(operation)
+    # ask for user input
+    # display result
+    # ask if user wants to retry
+    # if yes, repeat process
+    # if no, print "Thank you!"
