a3cc903bec3b9b7b903cf1f3ec7c872d3238ceef
Ms Lawliet <alexieanoya@gmail.com>
2023-05-22 23:12:48 +0800

    successfully created loop for retry

--- Non-OOP program code.py
+++ Non-OOP program code.py
@@ -1,33 +1,39 @@
 # Create a Simple App Calculator
 
-# ask for operation
-operation = input("Enter operation to be used: ")
-# ask for user input
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+while True:
+    # ask for operation
+    operation = input("Enter operation to be used: ")
+    # ask for user input
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 
-# perform operation
-# for addition,
-if operation == "addition":
-    sum = num1 + num2
-    print(f"The sum is {sum}.")
-# for subtraction,
-elif operation == "subtraction":
-    difference = num1 - num2
-    print(f"The difference is {difference}.")
-# for multiplication,
-elif operation == "multiplication":
-    product = num1 * num2
-    print(f"The product is {product}.")
-# for division,
-elif operation == "division":
-    quotient = num1 / num2
-    print(f"The quotient is {quotient}.")
-# else, invalid operation
-else:
-    print("Operation is invalid.")
+    # perform operation
+    # for addition,
+    if operation == "addition":
+        sum = num1 + num2
+        print(f"The sum is {sum}.")
+    # for subtraction,
+    elif operation == "subtraction":
+        difference = num1 - num2
+        print(f"The difference is {difference}.")
+    # for multiplication,
+    elif operation == "multiplication":
+        product = num1 * num2
+        print(f"The product is {product}.")
+    # for division,
+    elif operation == "division":
+        quotient = num1 / num2
+        print(f"The quotient is {quotient}.")
+    # else, invalid operation
+    else:
+        print("Operation is invalid.")
 
-# ask if user wants to retry
-retry = input("Do you want to try again? (yes or no) ").lower()
-# if yes, repeat process
-# if no, print "Thank you!"
+    # ask if user wants to retry
+    retry = input("Do you want to try again? (yes or no) ").lower()
+    # if yes, repeat process
+    if retry == "yes":
+        continue
+    # if no, print "Thank you!"
+    elif retry == "no":
+        print("Thank you!")
+        break
