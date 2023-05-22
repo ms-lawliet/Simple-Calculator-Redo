5ecc38b4b857a536d73f80b029f2eecc87ad25c7
Ms Lawliet <alexieanoya@gmail.com>
2023-05-22 23:51:15 +0800

    asked user for two numbers

--- OOP program code.py
+++ OOP program code.py
@@ -5,6 +5,8 @@ ui = UserInterface()
 
 # ask for input by calling methods
 operation = ui.ask_operation()
+num1 = ui.ask_num1()
+num2 = ui.ask_num2()
 # create instances for operations
 # call method to print result
 # call method for retry
--- UserInterface.py
+++ UserInterface.py
@@ -2,8 +2,18 @@ class UserInterface:
     def ask_operation(self):
         # ask for operation
         operation = input("Which operation do you want to perform? ").lower()
-        print(operation)
-    # ask for user input
+        return operation
+
+    def ask_num1(self):
+        # ask for user input
+        num1 = float(input("Enter first number: "))
+        print(num1)
+
+    def ask_num2(self):
+        # ask for user input
+        num2 = float(input("Enter first number: "))
+        print(num2)
+
     # display result
     # ask if user wants to retry
     # if yes, repeat process
