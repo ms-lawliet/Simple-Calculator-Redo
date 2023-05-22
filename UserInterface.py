78d84820fe5fc508f6df2ca3f00e71fa3319095a
Ms Lawliet <alexieanoya@gmail.com>
2023-05-23 01:19:45 +0800

    applied exception handling

--- UserInterface.py
+++ UserInterface.py
@@ -6,13 +6,19 @@ class UserInterface:
 
     def ask_num1(self):
         # ask for user input
-        num1 = float(input("Enter first number: "))
-        return num1
+        try:
+            num1 = float(input("Enter first number: "))
+            return num1
+        except ValueError:
+            return "Invalid input."
 
     def ask_num2(self):
         # ask for user input
-        num2 = float(input("Enter second number: "))
-        return num2
+        try:
+            num2 = float(input("Enter second number: "))
+            return num2
+        except ValueError:
+            return "Invalid input."
 
     # display result based on chosen operation
     def print_result(self, operation, sum, difference, product, quotient):
