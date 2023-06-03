141f9d0e67b44d064575386b6009e6301634cc8e
Ms Lawliet <alexieanoya@gmail.com>
2023-06-03 15:56:24 +0800

    overwritten division function so new result is rounded off

--- ImprovedCalc.py
+++ ImprovedCalc.py
@@ -3,6 +3,17 @@ from colorama import Fore, Back, Style
 
 
 class ImprovedCalculator(SimpleCalculator):
+    # for division
+    def divide(self, num1, num2):
+        try:
+            quotient = round(num1 / num2, 2)
+            return quotient
+        except TypeError:
+            return Fore.RED + Back.RESET + Style.BRIGHT + "Cannot perform operation due to invalid input."
+        except ZeroDivisionError:
+            return Fore.RED + Back.RESET + Style.BRIGHT + "Zero Division Error."
+
+    # for power
     def power(self, num1, num2):   # raise num1 to the power of num2
         try:
             power = num1 ** num2
@@ -10,6 +21,7 @@ class ImprovedCalculator(SimpleCalculator):
         except TypeError:
             return Fore.RED + Back.RESET + Style.BRIGHT + "Cannot perform operation due to invalid input."
 
+    # for nth_root
     def nth_root(self, num1, num2):    # find the square root, cube root, fourth root, etc.
         try:
             nth_root = num1 ** (1/num2)  # num2 represents the nth root
